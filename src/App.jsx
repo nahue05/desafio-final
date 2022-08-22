37f446e04a011b58397f91e3ad913f46b2bfd413
nahuel <christiannahuelberrutti25@gmail.com>
2022-08-22 20:51:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Home</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Precios
+                  <span className="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">carrito</a>
+              </li>
+              
+              
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-sm-2" type="text" placeholder="Search" />
+              <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+    
+  );
+}
+
+export default App;
